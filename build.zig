98b3bb4c1f52690b9e5dcfd92167e46359dc7b9e
Louis Pearson <opensource@louispearson.work>
2023-03-15 18:03:16 -0600

    Fix addModule

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "win32",
+    _ = b.addModule("win32", .{
         .source_file = .{ .path = "win32.zig" },
     });
 }
